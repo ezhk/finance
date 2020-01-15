07979282d5d0350c30a3bd3374179fb7cf62fc64
Andrey Kiselev <ezhik@ezhik.info>
2020-01-15 10:53:58 +0300

    initial web UI commit

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <header>
+      <router-link class="header-title" to="/">Personal finances</router-link>
+      <user-login ref="login"></user-login>
+    </header>
+
+    <div class="top">
+      <keep-alive>
+        <router-view :key="$route.fullPath"></router-view>
+      </keep-alive>
+    </div>
+    <footer class="site-footer">
+      <a href="https://github.com/ezhk">Andrey Kiselev</a> &copy;&nbsp;2020
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+  // components: {
+  //   HelloWorld
+  // }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+header,
+footer {
+  margin: 5px 0 5px;
+  padding: 0 5px;
+  height: 45px;
+
+  border-radius: 5px;
+  border: 0.5px solid darkgray;
+
+  align-items: center;
+
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+header {
+  display: flex !important;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding-left: 15px;
+
+  font-size: 1.3rem;
+  font-family: monospace;
+  color: goldenrod;
+}
+
+.header-title:any-link {
+  color: unset;
+  text-decoration: none;
+}
+
+footer {
+  padding-right: 15px;
+  text-align: right;
+
+  line-height: 45px;
+}
+
+footer > a {
+  color: unset;
+  text-decoration: none;
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1000px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+}
+
+.top {
+  flex-grow: 1;
 }
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import Common from './components/Common.vue';
 
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    // main code URLs
+    {
+      path: '/',
+      component: Common,
+    },
+  ],
+});
+
+Vue.use(VueRouter);
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
